803aa8cd907687f048d39f51acf8faf2f5ec323c
Reim <timereim@gmail.com>
2022-05-20 18:54:28 +0300

    append '\n' to event_controller.go

--- internal/infra/http/controllers/event_controller.go
+++ internal/infra/http/controllers/event_controller.go
@@ -23,17 +23,17 @@ func (c *EventController) FindAll() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		events, err := (*c.service).FindAll()
 		if err != nil {
-			fmt.Printf("EventController.FindAll(): %s", err)
+			fmt.Printf("EventController.FindAll(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindAll(): %s", err)
+				fmt.Printf("EventController.FindAll(): %s\n", err)
 			}
 			return
 		}
 
 		err = success(w, events)
 		if err != nil {
-			fmt.Printf("EventController.FindAll(): %s", err)
+			fmt.Printf("EventController.FindAll(): %s\n", err)
 		}
 	}
 }
@@ -42,26 +42,26 @@ func (c *EventController) FindOne() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 		if err != nil {
-			fmt.Printf("EventController.FindOne(): %s", err)
+			fmt.Printf("EventController.FindOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindOne(): %s", err)
+				fmt.Printf("EventController.FindOne(): %s\n", err)
 			}
 			return
 		}
 		event, err := (*c.service).FindOne(int(id))
 		if err != nil {
-			fmt.Printf("EventController.FindOne(): %s", err)
+			fmt.Printf("EventController.FindOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindOne(): %s", err)
+				fmt.Printf("EventController.FindOne(): %s\n", err)
 			}
 			return
 		}
 
 		err = success(w, event)
 		if err != nil {
-			fmt.Printf("EventController.FindOne(): %s", err)
+			fmt.Printf("EventController.FindOne(): %s\n", err)
 		}
 	}
 }
@@ -70,44 +70,44 @@ func (c *EventController) FindByCoords() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		latitude, err := strconv.ParseFloat(chi.URLParam(r, "latitude"), 64)
 		if err != nil {
-			fmt.Printf("EventController.FindByCoords(): %s", err)
+			fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindByCoords(): %s", err)
+				fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			}
 			return
 		}
 		longitude, err := strconv.ParseFloat(chi.URLParam(r, "longitude"), 64)
 		if err != nil {
-			fmt.Printf("EventController.FindByCoords(): %s", err)
+			fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindByCoords(): %s", err)
+				fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			}
 			return
 		}
 		radius, err := strconv.ParseFloat(chi.URLParam(r, "radius"), 64)
 		if err != nil {
-			fmt.Printf("EventController.FindByCoords(): %s", err)
+			fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindByCoords(): %s", err)
+				fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			}
 			return
 		}
 		events, err := (*c.service).FindByCoords(latitude, longitude, radius)
 		if err != nil {
-			fmt.Printf("EventController.FindByCoords(): %s", err)
+			fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.FindByCoords(): %s", err)
+				fmt.Printf("EventController.FindByCoords(): %s\n", err)
 			}
 			return
 		}
 
 		err = success(w, events)
 		if err != nil {
-			fmt.Printf("EventController.FindAll(): %s", err)
+			fmt.Printf("EventController.FindAll(): %s\n", err)
 		}
 	}
 }
@@ -116,26 +116,26 @@ func (c *EventController) PostOne() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			fmt.Printf("EventController.PostOne(): %s", err)
+			fmt.Printf("EventController.PostOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.PostOne(): %s", err)
+				fmt.Printf("EventController.PostOne(): %s\n", err)
 			}
 			return
 		}
 		err = (*c.service).PostOne(body)
 		if err != nil {
-			fmt.Printf("EventController.PostOne(): %s", err)
+			fmt.Printf("EventController.PostOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.PostOne(): %s", err)
+				fmt.Printf("EventController.PostOne(): %s\n", err)
 			}
 			return
 		}
 
 		err = success(w, "posted")
 		if err != nil {
-			fmt.Printf("EventController.PostOne(): %s", err)
+			fmt.Printf("EventController.PostOne(): %s\n", err)
 		}
 	}
 }
@@ -144,36 +144,36 @@ func (c *EventController) UpdateOne() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 		if err != nil {
-			fmt.Printf("EventController.UpdateOne(): %s", err)
+			fmt.Printf("EventController.UpdateOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.UpdateOne(): %s", err)
+				fmt.Printf("EventController.UpdateOne(): %s\n", err)
 			}
 			return
 		}
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			fmt.Printf("EventController.UpdateOne(): %s", err)
+			fmt.Printf("EventController.UpdateOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.UpdateOne(): %s", err)
+				fmt.Printf("EventController.UpdateOne(): %s\n", err)
 			}
 			return
 		}
 
 		err = (*c.service).UpdateOne(int(id), body)
 		if err != nil {
-			fmt.Printf("EventController.UpdateOne(): %s", err)
+			fmt.Printf("EventController.UpdateOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.UpdateOne(): %s", err)
+				fmt.Printf("EventController.UpdateOne(): %s\n", err)
 			}
 			return
 		}
 
 		err = success(w, "updated")
 		if err != nil {
-			fmt.Printf("EventController.UpdateOne(): %s", err)
+			fmt.Printf("EventController.UpdateOne(): %s\n", err)
 		}
 	}
 }
@@ -182,27 +182,27 @@ func (c *EventController) DeleteOne() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 		if err != nil {
-			fmt.Printf("EventController.DeleteOne(): %s", err)
+			fmt.Printf("EventController.DeleteOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.DeleteOne(): %s", err)
+				fmt.Printf("EventController.DeleteOne(): %s\n", err)
 			}
 			return
 		}
 
 		err = (*c.service).DeleteOne(int(id))
 		if err != nil {
-			fmt.Printf("EventController.DeleteOne(): %s", err)
+			fmt.Printf("EventController.DeleteOne(): %s\n", err)
 			err = internalServerError(w, err)
 			if err != nil {
-				fmt.Printf("EventController.DeleteOne(): %s", err)
+				fmt.Printf("EventController.DeleteOne(): %s\n", err)
 			}
 			return
 		}
 
 		err = success(w, "deleted")
 		if err != nil {
-			fmt.Printf("EventController.DeleteOne(): %s", err)
+			fmt.Printf("EventController.DeleteOne(): %s\n", err)
 		}
 	}
 }
